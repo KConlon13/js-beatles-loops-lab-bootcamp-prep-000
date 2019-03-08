7570b176f4883acb567b39fc10db0bfb19423202
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-08 03:10:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function theBeatlesPlay (musicians, instruments) {
 
 function johnLennonFacts () {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var moreFacts = [];
+  var newFacts = [];
   var i = 0;
   while (i < facts.length) {
-    moreFacts.push(facts[i] +"!!!");
+    newFacts.push(facts[i] +"!!!");
     i++;
   }
-  return moreFacts;
+  return newFacts;
 }
