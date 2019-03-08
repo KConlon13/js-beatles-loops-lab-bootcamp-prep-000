7b6942d8998a679e4816a5938e8b5119a5f93d43
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-08 02:54:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function theBeatlesPlay (musicians, instruments) {
   for (var i = 0; i < musicians.length; i++){
     allMusicians.push(musicians[i] + " plays " + instruments[i]);
     }
-    return theBeatlesPlay;
+    return allMusicians;
   }
-  
-theBeatlesPlay;
+
   
