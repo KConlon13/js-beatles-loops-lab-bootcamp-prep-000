ced5d3dc4861f0e83bd6b1b16cd9edf14a2c1a9d
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-08 02:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay (musicians, instruments) {
   var allMusicians = [];
   for (var i = 0; i < musicians.length; i++){
-    allMusicians.push(musicians[i] + " plays " + instruments[i])
+    allMusicians.push(musicians[i] + " plays " + instruments[i]);
     }
     return theBeatlesPlay;
   }
