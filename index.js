965f83e3771620d4c5b3b81b6f6d85bb5219733c
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-08 02:49:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // add solution here
-var array = [];
 
-function theBeatlesPlay (musician, instrument) {
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["guitar", "bass", "lead guitar", "drums"]
+
+function theBeatlesPlay (musicians, instruments) {
   for (var i = 0; i < musicians.length; i++){
-    console.log(`${musicians} plays ${instrument}`)
+    console.log(`${musicians} plays ${instruments}`)
     }
     return theBeatlesPlay;
   }
